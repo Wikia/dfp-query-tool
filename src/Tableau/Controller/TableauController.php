4d332e92cc8653b71575613137a2984ee581a30c
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-11-03 11:46:59 +0100

    ADEN-4175 Refactor template

--- src/Tableau/Controller/TableauController.php
+++ src/Tableau/Controller/TableauController.php
@@ -7,6 +7,32 @@ use Common\Controller\Controller;
 class TableauController extends Controller
 {
 	public function renderWebConnector() {
-		return $this->render('tableau-web-connector');
+		$dimensions = [
+			'adUnitName' => 'Ad Unit name',
+			'country' => 'Country',
+			'creativeId' => 'Creative ID',
+			'creativeName' => 'Creative name',
+			'creativeSize' => 'Creative size',
+			'date' => 'Date',
+			'deviceCategory' => 'Device category',
+			'keyValues' => 'Key-values',
+			'lineItemId' => 'Line item ID',
+			'lineItemName' => 'Line item name',
+			'orderId' => 'Order ID',
+			'orderName' => 'Order name'
+		];
+
+		$metrics = [
+			'totalActiveViewEligibleImpressions' => 'Total Active View eligible impressions',
+			'totalActiveViewMeasurableImpressions' => 'Total Active View measurable impressions',
+			'totalActiveViewViewableImpressions' => 'Total Active View viewable impressions',
+			'totalActiveViewMeasurableImpressionsRate' => 'Total Active View % measurable impressions',
+			'totalActiveViewViewableImpressionsRate' => 'Total Active View % viewable impressions',
+		];
+
+		return $this->render('tableau-web-connector', [
+			'dimensions' => $dimensions,
+			'metrics' => $metrics
+		]);
 	}
 }
