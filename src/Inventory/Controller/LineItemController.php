1fd51f52ee2a3a9f67a868c79b9519754da70291
Bogna Knychala <bogna.ka@gmail.com>
2016-11-17 14:29:13 +0100

    ADEN-4240 | revert ws changes

--- src/Inventory/Controller/LineItemController.php
+++ src/Inventory/Controller/LineItemController.php
@@ -9,43 +9,45 @@ use Inventory\Api\LineItemService;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 
-class LineItemController extends Controller {
+class LineItemController extends Controller
+{
 	protected $lineItemService;
 	protected $lineItemCreativeAssociationService;
 
-	public function __construct( Application $app ) {
-		parent::__construct( $app );
+	public function __construct(Application $app)
+	{
+		parent::__construct($app);
 		$this->lineItemService = new LineItemService();
 		$this->lineItemCreativeAssociationService = new LineItemCreativeAssociationService();
 	}
 
-	public function createLineItem( Request $request ) {
+	public function createLineItem(Request $request) {
 		$lineItem = null;
 		$lineItemCreativeAssociation = [];
 		$message = null;
 		$messageType = 'info';
-		$form = [ ];
+		$form = [];
 
-		if ( $request->isMethod( 'POST' ) ) {
+		if ($request->isMethod('POST')) {
 			$form = $request->request->all();
 			try {
-				$lineItem = $this->lineItemService->create( $form );
-				$lineItemCreativeAssociation = $this->lineItemCreativeAssociationService->create( $form['creativeId'], $lineItem['id'] );
+				$lineItem = $this->lineItemService->create($form);
+				$lineItemCreativeAssociation = $this->lineItemCreativeAssociationService->create($form['creativeId'], $lineItem['id']);
 				$messageType = 'success';
 				$message = 'Line items successfully created.';
-			} catch ( LineItemException $exception ) {
+			} catch (LineItemException $exception) {
 				$message = $exception->getMessage();
 				$messageType = 'danger';
 			}
 		}
 
-		return $this->render( 'line-item', [
+		return $this->render('line-item', [
 			'action' => $request->getUri(),
 			'lineItem' => $lineItem,
 			'lica' => $lineItemCreativeAssociation,
 			'message' => $message,
 			'messageType' => $messageType,
-			'form' => json_encode( $form )
-		] );
+			'form' => json_encode($form)
+		]);
 	}
 }
