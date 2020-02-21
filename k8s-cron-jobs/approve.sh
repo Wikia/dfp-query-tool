0b6fea321ae11047fb7de605a90de358eba017b4
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2020-02-21 08:22:01 +0100

    Fix path to credentials.
    It was broken when setting user to nobody.
    Previously credentials from home dir were used.

--- k8s-cron-jobs/approve.sh
+++ k8s-cron-jobs/approve.sh
@@ -1,2 +1,2 @@
-cp /etc/adeng-query-tool-credentials/adsapi_php.ini /home/maka/auth.ini
+cp /etc/adeng-query-tool-credentials/adsapi_php.ini /home/maka/dfp-query-tool/config/auth.ini
 app/console suggested-adunits:approve
