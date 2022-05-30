689cfc8bafca5f21e03b1b2b7f9e863c7f87219e
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-05-30 17:58:04 +0200

    ADEN-11842 add failing test

--- src/Inventory/Api/CustomTargetingServiceTest.php
+++ src/Inventory/Api/CustomTargetingServiceTest.php
@@ -4,6 +4,34 @@ namespace Inventory\Api;
 use PHPUnit\Framework\TestCase;
 
 class CustomTargetingServiceTest extends TestCase {
+    public function testGetKeyIds() {
+        $customTargetingService = new CustomTargetingService();
+
+        $this->assertSame(
+            [],
+            $customTargetingService->getKeyIds([]),
+            "Failed empty arrays case"
+        );
+
+        $valueMock = $this->createMock(\Google\AdsApi\AdManager\v202105\CustomTargetingValue::class);
+        $valueMock->method('getId')
+            ->willReturn(666);
+
+        $pageMock = $this->createMock(\Google\AdsApi\AdManager\v202105\CustomTargetingValuePage::class);
+        $pageMock->method('getResults')
+            ->willReturn([$valueMock]);
+
+        $customTargetingServiceMock = $this->createStub(\Google\AdsApi\AdManager\v202105\CustomTargetingService::class);
+        $customTargetingServiceMock->method('getCustomTargetingKeysByStatement')
+            ->willReturn($pageMock);
+
+        $this->assertSame(
+            [666],
+            $customTargetingService->getKeyIds(['src']),
+            "Failed empty arrays case"
+        );
+    }
+
     public function testGetValuesIdsFromMap() {
         $customTargetingService = new CustomTargetingService();
 
