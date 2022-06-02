1c977ef5aa90e6b0af1d3518b3d50af49aa6de23
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-06-02 15:23:26 +0200

    ADEN-11842 add new failing test case

--- src/Inventory/Api/LineItemServiceTest.php
+++ src/Inventory/Api/LineItemServiceTest.php
@@ -59,7 +59,7 @@ class LineItemServiceTest extends TestCase {
         );
     }
 
-    private function createLineItemMock($getCustomTargetingMock = null) {
+    private function createLineItemMock($getCustomTargetingMock = null, $getIdMock = 1234567890, $getOrderIdMock = 9876543210) {
         $targetingMock = $this->createStub(\Google\AdsApi\AdManager\v202105\Targeting::class);
         $targetingMock->method('getCustomTargeting')
             ->willReturn($getCustomTargetingMock);
@@ -69,10 +69,10 @@ class LineItemServiceTest extends TestCase {
             ->willReturn($targetingMock);
 
         $lineItemMock->method('getId')
-            ->willReturn(1234567890);
+            ->willReturn($getIdMock);
 
         $lineItemMock->method('getOrderId')
-            ->willReturn(9876543210);
+            ->willReturn($getOrderIdMock);
 
         return $lineItemMock;
     }
@@ -95,10 +95,12 @@ class LineItemServiceTest extends TestCase {
         );
     }
 
-    private function createCustomCriteriaSetMock() {
+    private function createCustomCriteriaSetMock($getKeyIdMock = 666, $getValueIdsMock = []) {
         $customCriteriaMock = $this->createStub(\Google\AdsApi\AdManager\v202105\CustomCriteria::class);
         $customCriteriaMock->method('getKeyId')
-            ->willReturn(666);
+            ->willReturn($getKeyIdMock);
+        $customCriteriaMock->method('getValueIds')
+            ->willReturn($getValueIdsMock);
 
         $customCriteriaNodeMock = $this->createStub(\Google\AdsApi\AdManager\v202105\CustomCriteriaSet::class);
         $customCriteriaNodeMock->method('getChildren')
@@ -137,6 +139,46 @@ class LineItemServiceTest extends TestCase {
 
         $this->assertSame(
             [],
+            $lineItemService->findLineItemIdsByKeyValues(123, [123, 456]),
+            "Failed no matching line-items test case"
+        );
+    }
+
+    public function testFindLineItemIdsByKeyValues_forLineItemsWithTargetingWithTheKeyVal() {
+        $keyIdMock = 123;
+        $valuesIdsMockForFirstLineItemMock = [231, 321, 456, 564, 654];
+        $valuesIdsMockForSecondLineItemMock = [123, 321];
+        $secondLineItemIdMock = 1122334455;
+        $secondOrderIdMock = 6677889900;
+
+        $pageMock = $this->createLineItemPageMock([
+            $this->createLineItemMock($this->createCustomCriteriaSetMock()),
+            $this->createLineItemMock(
+                $this->createCustomCriteriaSetMock($keyIdMock, $valuesIdsMockForFirstLineItemMock)
+            ),
+            $this->createLineItemMock($this->createCustomCriteriaSetMock()),
+            $this->createLineItemMock(
+                $this->createCustomCriteriaSetMock($keyIdMock, $valuesIdsMockForSecondLineItemMock),
+                $secondLineItemIdMock,
+                $secondOrderIdMock
+            ),
+        ], 4);
+        $lineItemServiceMock = $this->createLineItemServiceMock($pageMock);
+        $lineItemService = new LineItemService($this->createNetworkServiceMock(), $lineItemServiceMock);
+
+        $this->assertSame(
+            [
+                [
+                    'line_item_id' => 1234567890,
+                    'order_id' => 9876543210,
+                    'found_value_id' => 456,
+                ],
+                [
+                    'line_item_id' => $secondLineItemIdMock,
+                    'order_id' => $secondOrderIdMock,
+                    'found_value_id' => 123,
+                ]
+            ],
             $lineItemService->findLineItemIdsByKeyValues(123, [123, 456]),
             "Failed no matching line-items test case"
         );
