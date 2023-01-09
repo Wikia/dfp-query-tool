0398d9f54e6f159b74acd63c37b856552068c57f
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-01-09 13:34:51 +0100

    ADEN-12510 add possibility to skip first row

--- src/Code/Command/GenerateBiddersSlotsJsonCommand.php
+++ src/Code/Command/GenerateBiddersSlotsJsonCommand.php
@@ -3,6 +3,7 @@
 namespace Code\Command;
 
 use Knp\Command\Command;
+use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,7 +23,8 @@ class GenerateBiddersSlotsJsonCommand extends Command
             ->setDescription('Creates JSON for given bidder based on given input file')
             ->addOption('bidder', 'b', InputOption::VALUE_REQUIRED, 'Name of the bidder')
             ->addOption('csv', 'f', InputOption::VALUE_REQUIRED, 'CSV file with IDs')
-            ->addOption( 'separator', 's', InputOption::VALUE_OPTIONAL, 'CSV file separator (default: ',')', ',');
+            ->addOption( 'separator', 's', InputOption::VALUE_OPTIONAL, 'CSV file separator (default: \',\')', ',')
+            ->addOption( 'skip-first-row', 'skip', InputOption::VALUE_OPTIONAL, 'Skip first row (default: true) - most of the times these are headers', true);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -30,13 +32,14 @@ class GenerateBiddersSlotsJsonCommand extends Command
         $bidderName = $input->getOption('bidder');
         $csvFile = $input->getOption('csv');
         $csvSeparator = $input->getOption('separator');
+        $skipFirstRow = $input->getOption('skip-first-row');
 
         $isValid = $this->validate($output, $bidderName, $csvFile);
 
         if( $isValid ) {
             $csv = $this->getCsvContentAsArray($csvFile, $csvSeparator);
             $output->writeln(sprintf('Generated slots JSON for %s bidder:', $bidderName));
-            $output->writeln($this->generateJSON($csv));
+            $output->writeln($this->generateJSON($csv, $skipFirstRow));
         }
     }
 
@@ -60,10 +63,14 @@ class GenerateBiddersSlotsJsonCommand extends Command
         }, file($csvFile));
     }
 
-    private function generateJSON(array $csvContent): string {
+    private function generateJSON(array $csvContent, bool $skipFirstRow): string {
         $slotConfig = [];
 
-        foreach($csvContent as $row) {
+        foreach($csvContent as $rowNo => $row) {
+            if ($skipFirstRow && $rowNo === 0 ) {
+                continue;
+            }
+
             $slotName = $row[0];
             $slotSizes = [$row[1], $row[2]];
             $slotBidderId = $row[3];
